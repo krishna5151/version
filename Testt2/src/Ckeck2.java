c9d4605d9fa4cd72c9698763afcdf8b8704e5566
krishna5151 <krishnagkrishna333@gmail.com>
2016-12-19 11:33:59 +0530

    Its my first commit msg..

---
+++ Testt2/src/Ckeck2.java
@@ -0,0 +1,9 @@
+
+public class Ckeck2 {
+
+	public static void main(String[] args) {
+	System.out.println("Hai Krishna v2");
+
+	}
+
+}
---
+++ Testt3/src/Check3.java
@@ -0,0 +1,9 @@
+
+public class Check3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hai Krishna V3");
+
+	}
+
+}
